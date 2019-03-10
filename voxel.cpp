9f99da981e93c3427315b161b2fec90a5e03ae81
Tony Mamacos <tmamacos@gmail.com>
2019-03-10 15:35:01 +0200

    Literal last try

--- voxel.cpp
+++ voxel.cpp
@@ -18,7 +18,7 @@ int frame = 0;
 #endif
 
 #define CACHESIZE 3
-#define MAPSIZE  map_height*map_width
+#define MAPSIZE map_height *map_width
 
 int8_t cmap[CACHESIZE];
 bool ccolor[CACHESIZE];
@@ -26,37 +26,40 @@ int cmapOffset = -1;
 
 void render(ScreenBuff *screenBuff)
 {
-int cachehit = 0;
-int cachemiss = 0;
+    int cachehit = 0;
+    int cachemiss = 0;
 
 #ifdef INTERLACE
     frame += 1;
-    if (frame == 2) frame = 0;
+    if (frame == 2)
+        frame = 0;
 #endif
 
     FIXPOINT fsinang = FIXPOINT_SIN(p.fangle);
     FIXPOINT fcosang = FIXPOINT_COS(p.fangle);
 
     int hiddeny[screenBuff->WIDTH];
-    for(int i=0; i<screenBuff->WIDTH; i+=1) {
+    for (int i = 0; i < screenBuff->WIDTH; i += 1)
+    {
         hiddeny[i] = screenBuff->HEIGHT;
 #ifdef INTERLACE
-        if (i%2 == frame) drawVertLine2(screenBuff, i, 0, screenBuff->HEIGHT, 0);
+        if (i % 2 == frame)
+            drawVertLine2(screenBuff, i, 0, screenBuff->HEIGHT, 0);
 #endif
     }
 
     FIXPOINT fdeltaz = FIXP_1;
     // Draw from front to back
-    for(FIXPOINT fz=FIXP_1; fz<p.fdistance; fz+=fdeltaz)
+    for (FIXPOINT fz = FIXP_1; fz < p.fdistance; fz += fdeltaz)
     {
         // 90 degree field of view
-        FIXPOINT fsinfz = FIXP_MULT(fsinang , fz);
-        FIXPOINT fcosfz = FIXP_MULT(fcosang , fz);
+        FIXPOINT fsinfz = FIXP_MULT(fsinang, fz);
+        FIXPOINT fcosfz = FIXP_MULT(fcosang, fz);
 
-        FIXPOINT fplx = FIXP_MULT(-fcosang , fz) - fsinfz;
+        FIXPOINT fplx = FIXP_MULT(-fcosang, fz) - fsinfz;
         FIXPOINT fply = fsinfz - fcosfz;
         FIXPOINT fprx = fcosfz - fsinfz;
-        FIXPOINT fpry = FIXP_MULT(-fsinang , fz) - fcosfz;
+        FIXPOINT fpry = FIXP_MULT(-fsinang, fz) - fcosfz;
         FIXPOINT fdx = (fprx - fplx) / screenBuff->WIDTH;
         FIXPOINT fdy = (fpry - fply) / screenBuff->WIDTH;
         FIXPOINT finvz = FIXP_DIV(FIXP_1, fz) * 240;
@@ -65,36 +68,46 @@ int cachemiss = 0;
         fply += p.fy;
 
 #ifdef INTERLACE
-        for(int i=frame; i<screenBuff->WIDTH; i+=2)
+        for (int i = frame; i < screenBuff->WIDTH; i += 2)
 #else
-        for(int i=0; i<screenBuff->WIDTH; i+=1)
+        for (int i = 0; i < screenBuff->WIDTH; i += 1)
 #endif
         {
-            int mapoffset = ((FIXP_INT_PART(FIXP_DIV(fply,p.mapScaleFactor)) & p.mapwidthperiod) << p.shift) + (FIXP_INT_PART(FIXP_DIV(fplx,p.mapScaleFactor)) & p.mapheightperiod);
-            if (((cmapOffset <= mapoffset) && (cmapOffset+CACHESIZE > mapoffset))) // || ( cmapOffset+CACHESIZE % MAPSIZE > mapoffset && cmapOffset >= 0))
+            if (hiddeny[i] >= -5)
             {
-                cachehit += 1;
-            }
-            else {
-                cmapOffset = mapoffset;
-                if (mapoffset + CACHESIZE > MAPSIZE) {
-                    memcpy(cmap, map_data + mapoffset*sizeof(int8_t), (mapoffset + CACHESIZE - MAPSIZE)*sizeof(int8_t)); 
-                    memcpy(ccolor, map_colour + mapoffset*sizeof(bool), (mapoffset + CACHESIZE - MAPSIZE)*sizeof(bool));
-                    // memcpy(cmap + (mapoffset + CACHESIZE - MAPSIZE)*sizeof(int8_t), map_data, (MAPSIZE - mapoffset + CACHESIZE)*sizeof(int8_t)); 
-                    // memcpy(ccolor + (mapoffset + CACHESIZE - MAPSIZE)*sizeof(int8_t), map_colour, (MAPSIZE - mapoffset + CACHESIZE)*sizeof(bool));
-                } else {
-                    memcpy(cmap, map_data + mapoffset*sizeof(int8_t), CACHESIZE*sizeof(int8_t)); 
-                    memcpy(ccolor, map_colour + mapoffset*sizeof(bool), CACHESIZE*sizeof(bool));
+                int mapoffset = ((FIXP_INT_PART(FIXP_DIV(fply, p.mapScaleFactor)) & p.mapwidthperiod) << p.shift) + (FIXP_INT_PART(FIXP_DIV(fplx, p.mapScaleFactor)) & p.mapheightperiod);
+                if (((cmapOffset <= mapoffset) && (cmapOffset + CACHESIZE > mapoffset))) // || ( cmapOffset+CACHESIZE % MAPSIZE > mapoffset && cmapOffset >= 0))
+                {
+                    cachehit += 1;
                 }
-                cachemiss++;
-            }
-            
-            int heightonscreen = (FIXP_TO_INT((p.height - cmap[mapoffset-cmapOffset]) * finvz) + p.horizon);
-            drawVertLine2(screenBuff, i, heightonscreen, hiddeny[i], ccolor[mapoffset-cmapOffset]);
-            if (heightonscreen < hiddeny[i]) hiddeny[i] = heightonscreen;
+                else
+                {
+                    cmapOffset = mapoffset;
+                    if (mapoffset + CACHESIZE > MAPSIZE)
+                    {
+                        memcpy(cmap, map_data + mapoffset * sizeof(int8_t), (mapoffset + CACHESIZE - MAPSIZE) * sizeof(int8_t));
+                        memcpy(ccolor, map_colour + mapoffset * sizeof(bool), (mapoffset + CACHESIZE - MAPSIZE) * sizeof(bool));
+                        // memcpy(cmap + (mapoffset + CACHESIZE - MAPSIZE)*sizeof(int8_t), map_data, (MAPSIZE - mapoffset + CACHESIZE)*sizeof(int8_t));
+                        // memcpy(ccolor + (mapoffset + CACHESIZE - MAPSIZE)*sizeof(int8_t), map_colour, (MAPSIZE - mapoffset + CACHESIZE)*sizeof(bool));
+                    }
+                    else
+                    {
+                        memcpy(cmap, map_data + mapoffset * sizeof(int8_t), CACHESIZE * sizeof(int8_t));
+                        memcpy(ccolor, map_colour + mapoffset * sizeof(bool), CACHESIZE * sizeof(bool));
+                    }
+                    cachemiss++;
+                }
 
-            fplx += fdx;
-            fply += fdy;
+                int heightonscreen = (FIXP_TO_INT((p.height - cmap[mapoffset - cmapOffset]) * finvz) + p.horizon);
+                if (heightonscreen < hiddeny[i])
+                {
+                    drawVertLine2(screenBuff, i, heightonscreen, hiddeny[i], ccolor[mapoffset - cmapOffset]);
+                    hiddeny[i] = heightonscreen;
+                }
+
+                fplx += fdx;
+                fply += fdy;
+            }
         }
 
         fdeltaz += p.fdeltaMod;
@@ -103,8 +116,8 @@ int cachemiss = 0;
 
 void voxelInput(byte buttonVals, Point *p)
 {
-    p->fx -= FIXP_MULT(FIXPOINT_SIN(p->fangle) , p->fmove);
-    p->fy -= FIXP_MULT(FIXPOINT_COS(p->fangle) , p->fmove);
+    p->fx -= FIXP_MULT(FIXPOINT_SIN(p->fangle), p->fmove);
+    p->fy -= FIXP_MULT(FIXPOINT_COS(p->fangle), p->fmove);
 
     if (processKey(buttonVals, P1_Top))
     {
@@ -113,8 +126,8 @@ void voxelInput(byte buttonVals, Point *p)
     }
     if (processKey(buttonVals, P1_Bottom))
     {
-        p->fx += FIXP_MULT(FIXPOINT_SIN(p->fangle) , p->fmove);
-        p->fy += FIXP_MULT(FIXPOINT_COS(p->fangle) , p->fmove);
+        p->fx += FIXP_MULT(FIXPOINT_SIN(p->fangle), p->fmove);
+        p->fy += FIXP_MULT(FIXPOINT_COS(p->fangle), p->fmove);
     }
     if (processKey(buttonVals, P1_Left))
     {
@@ -143,8 +156,8 @@ void voxelInput(byte buttonVals, Point *p)
     }
 
     // Collision detection. Don't fly below the surface.
-    int mapoffset = (((int)(FIXP_INT_PART(p->fy/p->mapScaleFactor)) & (map_width-1)) << p->shift) + (((int)FIXP_INT_PART(p->fx/p->mapScaleFactor)) & (map_height-1));
-    if ((map_data[mapoffset]+10) > p->height)
+    int mapoffset = (((int)(FIXP_INT_PART(p->fy / p->mapScaleFactor)) & (map_width - 1)) << p->shift) + (((int)FIXP_INT_PART(p->fx / p->mapScaleFactor)) & (map_height - 1));
+    if ((map_data[mapoffset] + 10) > p->height)
         p->height = map_data[mapoffset] + 10;
 }
 
@@ -163,7 +176,7 @@ void voxelInit()
     p.mapwidthperiod = map_width - 1;
     p.mapheightperiod = map_height - 1;
 
-    p.fdistance = INT_TO_FIXP(700);
+    p.fdistance = INT_TO_FIXP(600);
     p.fx = INT_TO_FIXP(75);
     p.fy = INT_TO_FIXP(75);
     p.fangle = FLOAT_TO_FIXP(-0.6);
@@ -173,7 +186,7 @@ void voxelInit()
     p.fturn = FLOAT_TO_FIXP(0.1);
 
     p.height = 60;
-    p.horizon = 15;
+    p.horizon = -30;
     p.shift = 7;
     p.mapScaleFactor = INT_TO_FIXP(8);
 }
